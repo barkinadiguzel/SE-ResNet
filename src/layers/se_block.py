034116c0bbd75e63d73c8a4c5ed353f8f8d0b949
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-02 11:11:00 +0300

    Create se_block.py

---
+++ src/layers/se_block.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SEBlock(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super().__init__()
+        self.fc1 = nn.Linear(channels, channels // reduction, bias=False)
+        self.fc2 = nn.Linear(channels // reduction, channels, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = x.mean((2,3))          
+        y = self.fc1(y)
+        y = self.relu(y)
+        y = self.fc2(y)
+        y = self.sigmoid(y).view(b, c, 1, 1)
+        return x * y
